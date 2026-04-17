07f224ad6ab67cceeedb244199be9eb46d11468d
duongnguyen3003 <dangduong300306@gmail.com>
2026-04-17 17:03:04 +0700

    feat: add Book class

---
+++ AimsProject/src/hust/soict/globalict/aims/media/Book.java
@@ -0,0 +1,68 @@
+package hust.soict.globalict.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private String id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public Book() {
+    }
+
+    // Getters and Setters
+    public float getCost() {
+        return cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    // Method to add an author
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+            System.out.println("The author " + authorName + " has been added.");
+        } else {
+            System.out.println("The author " + authorName + " is already in the list!");
+        }
+    }
+
+    // Method to remove an author
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+            System.out.println("The author " + authorName + " has been removed.");
+        } else {
+            System.out.println("The author " + authorName + " was not found in the list.");
+        }
+    }
+}
